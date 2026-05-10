57b61a571d5d5e1124b3754e35f9b5e2c3747829
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2026-05-09 20:27:17 -0600

    Añadido menu de opciones disponibles para administradores/gestores de inventarios

--- app/src/main/java/com/grupo1/sgi_fia/controller/MenuActivity.java
+++ app/src/main/java/com/grupo1/sgi_fia/controller/MenuActivity.java
@@ -1,9 +1,6 @@
-package com.grupo1.sgi_fia;
+package com.grupo1.sgi_fia.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.grupo1.sgi_fia.R;
 
-    Button btnIniciarSesion;
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
-
-        btnIniciarSesion.setOnClickListener((view)->{
-
-            Intent menu = new Intent();
-
-
-                }
-        );
-
     }
 }
